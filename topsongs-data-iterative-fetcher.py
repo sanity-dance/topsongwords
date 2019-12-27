0addc48fc4562d2482de4eaa0dc2ee130c200952
Sanity Dance <mw@tdublu.com>
2019-12-27 09:32:09 -0800

    Updates spider to retrieve peak date and top ten entry date

--- topsongs-data-iterative-fetcher.py
+++ topsongs-data-iterative-fetcher.py
@@ -8,7 +8,7 @@ import time as t
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 cookies = cookies = { 'sessionid': '123..'}
 now = datetime.datetime.now()
-songdata = pd.DataFrame(columns=['Title','Artist','Peak','Number of weeks in top ten','Year'])
+songdata = pd.DataFrame(columns=['Top ten entry date','Title','Artist','Peak','Peak date','Number of weeks in top ten'])
 for i in range(1958,int(now.year)+1):
     html = requests.get('https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_top-ten_singles_in_' + str(i),cookies=cookies,headers=headers).text
     soup = BeautifulSoup(html, 'lxml')
@@ -18,7 +18,7 @@ for i in range(1958,int(now.year)+1):
     swiftcount = 0 # Keeps track of where repeat artists with rowspans greater than 1 should be inserted.
     drakecontingency = 0 # Used to keep track of how many rows have the same artist when the rowspan is greater than 1.
 
-    # The following code mines the first table found on the Wikipedia page and extracts only table elements that do not correspond to a month and date. We don't care about the exact month and day that the song achieved Top 10 glory or their peak date. Furthermore, the way Wikipedia's tables are formatted, there are occasionally multiple songs that were all entered on the same day, and that date is listed only once, which creates problems for our script later down the road.
+    # The following code mines the first table found on the Wikipedia page and extracts all table elements other than the annotations column.
 
     try:
         tds = my_table.findAll('td')
@@ -28,46 +28,40 @@ for i in range(1958,int(now.year)+1):
 
     for td in tds:
         try:
-            d = datetime.datetime.strptime(td.get_text()[:-1], "%B %d") # Check if the current string is a Month Day format.
-        except ValueError:
-            try:
-                d = datetime.datetime.strptime(td.get_text()[:td.get_text().rfind('(')-1], "%B %d") # Catch a rare case where a song from the previous year charted again.
-            except ValueError:
-                try:
-                    d = datetime.datetime.strptime(td.get_text()[:td.get_text().rfind('[')-1], "%B %d") # Catch a rare case where a song's top ten entry date has an annotation.
-                except ValueError:
-                    try:
-                        if td.attrs['rowspan']:
-                            taylorswift = td.get_text()[:-1]
-                            swiftcount = 5
-                            drakecontingency = int(td.attrs['rowspan']) - 1
-                    except KeyError:
-                        pass
-                    if td.get_text()[0] != '[' and td.get_text()[0] and td.get_text()[0].strip(): # Drop annotations column.
-                        if swiftcount == 1:
-                            drakecontingency = drakecontingency-1;
-                            # print(taylorswift + ' appended!')
-                            Titles.append(taylorswift)
-                            if drakecontingency == 0:
-                                taylorswift = ''
-                                swiftcount = 0
-                            else:
-                                swiftcount = 4;
-                        if swiftcount > 0:
-                            swiftcount = swiftcount-1;
-                        Titles.append(td.get_text()[:-1]) # Drop the newline character present at the end of every single entry.
+            if td.attrs['rowspan']:
+                taylorswift = td.get_text()[:-1]
+                swiftcount = 7
+                drakecontingency = int(td.attrs['rowspan']) - 1
+        except KeyError:
+            pass
+        if td.get_text()[0] != '[' and td.get_text()[0] and td.get_text()[0].strip(): # Drop annotations column.
+            if swiftcount == 1:
+                drakecontingency = drakecontingency-1;
+                # print(taylorswift + ' appended!')
+                Titles.append(taylorswift)
+                if drakecontingency == 0:
+                    taylorswift = ''
+                    swiftcount = 0
+                else:
+                    swiftcount = 6;
+            if swiftcount > 0:
+                swiftcount = swiftcount-1;
+            Titles.append(td.get_text()[:-1]) # Drop the newline character present at the end of every single entry.
 
     nptitles = np.asarray(Titles).copy()
-    numsongs = int(np.prod(nptitles.shape)/4) # Find out how many songs are in this list.
-    reshaped = nptitles.reshape(numsongs,4) # Reshape the array based on the number of songs and the number of columns.
+    numsongs = int(np.prod(nptitles.shape)/6) # Find out how many songs are in this list.
+    reshaped = nptitles.reshape(numsongs,6) # Reshape the array based on the number of songs and the number of columns.
     newdata = pd.DataFrame(reshaped)
-    newdata.columns = ['Title','Artist','Peak','Number of weeks in top ten']
+    newdata.columns = ['Top ten entry date','Title','Artist','Peak','Peak date','Number of weeks in top ten']
     newdata['Year'] = str(i)
     newdata['Title'] = [j[1:j.rfind('\"')] for j in newdata['Title']] # Drops quotation marks and annotations in Title.
+    for j in newdata['Top ten entry date']:
+        if '[' in j:
+            j = j[:j.rfind('[')-1] # Catch a rare case where there is an annotation in the top ten entry date.
     if(i == now.year):
         cleanweeks = []
-        for j in newdata['Number of weeks in top ten']: # This code searches the number of weeks in top ten column for asterisks and eliminates them. This is only a 
-            try:                                         # problem in the current year.
+        for j in newdata['Number of weeks in top ten']: # This code searches the number of weeks in top ten column for asterisks and eliminates them. 
+            try:                                         # This is only a problem in the current year.
                 cleanweeks.append(int(j[:j.rindex('*')]))
             except :
                 cleanweeks.append(int(j))
